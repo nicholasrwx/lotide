249dd9d575efb8bfde481f6c4445f3754bccfea3
nicholasrwx <digitalexistence18@gmail.com>
2021-01-07 03:28:18 +0000

    comparing key names to array values

---
+++ countOnly.js
@@ -0,0 +1,49 @@
+const assertEqual = function (actual, expected) {
+  let msg1 = `${actual} === ${expected}`;
+  let msg2 = `${actual} !== ${expected}`;
+
+  if (actual === expected) {
+    console.log(`Assertion passed: ${msg1}`);
+  }
+  console.assert(actual === expected, `Assertion failed: ${msg2}`);
+};
+
+const countOnly = function (allItems, itemsToCount) {
+  let newObject = {};
+
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      if (newObject[item]) {
+        newObject[item] += 1;
+      } else {
+        newObject[item] = 1;
+      }
+    }
+  }
+
+  return newObject;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
