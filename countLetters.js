e1c4a11857ff5657fd502e5eed2b71106c8ce9b8
nicholasrwx <digitalexistence18@gmail.com>
2021-01-07 04:48:23 +0000

    counting letters in a string

---
+++ countLetters.js
@@ -0,0 +1,41 @@
+const assertEqual = function (actual, expected) {
+  let msg1 = `${actual} === ${expected}`;
+  let msg2 = `${actual} !== ${expected}`;
+
+  if (actual === expected) {
+    console.log(`Assertion passed: ${msg1}`);
+  }
+  console.assert(actual === expected, `${msg2}`);
+};
+
+const countLetters = function (string) {
+  var newObject = {};
+
+  for (let letter of string) {
+    if (newObject[letter]) {
+      newObject[letter]++;
+    } else if (
+      (!newObject[letter] && letter === "a") ||
+      letter === "l" ||
+      letter === "8"
+    ) {
+      newObject[letter] = 1;
+    } else {
+      continue;
+    }
+  }
+  return newObject;
+};
+
+let string = "I'm always up late, well passed 8 p.m.";
+
+const result1 = countLetters(string);
+console.log(result1);
+
+assertEqual(result1["a"], 4); //pass
+assertEqual(result1["l"], 4); //pass
+assertEqual(result1["8"], 1); //pass
+assertEqual(result1[" "], undefined); //pass
+assertEqual(result1[","], undefined); //pass
+assertEqual(result1["."], undefined); //pass
+assertEqual(result1["'"], undefined); //pass
